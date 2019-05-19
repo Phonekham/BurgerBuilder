42840a31390817bc43edd40d2ba38620c58db732
Phonekham <phone.madridista@gmail.com>
2019-05-19 20:55:02 +0700

    Implement BurgerBuilder Containers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
-      // <div className="App">
-      //   <div className="App-header">
-      //     <img className="App-logo" alt="logo" />
-      //     <h2>Welcome to React</h2>
-      //   </div>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
     );
   }
 }
