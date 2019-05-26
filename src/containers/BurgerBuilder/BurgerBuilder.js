ebf737309470cb72835a1f4c367aaab3e44eee73
Phonekham <phonekham.dev@gmail.com>
2019-05-26 14:33:52 -0700

    170 Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios.order";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,7 +84,28 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You Continue");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice.toFixed(2),
+      customer: {
+        name: "Phone",
+        address: {
+          street: "pt",
+          zipcode: "1234",
+          country: "Laos"
+        },
+        email: "test@gmail.com"
+      },
+      deliveryMethod: "fast"
+    };
+    axios
+      .post("/order.json", order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   render() {
